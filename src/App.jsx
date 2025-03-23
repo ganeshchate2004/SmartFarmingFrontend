f717fefab1f43da2e7cf5a56f1623b8e4b21f017
ganeshchate2004 <ganeshchate2004@gmail.com>
2025-03-23 16:45:00 +0530

    auth change

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Nav from './Nav';
 import PlantPrediction from './PlantPrediction';
 import PricePrediction from './PricePrediction';
 import PredictDisease from './PredictDisease';
-import AuthForm from './authform';
+import AuthForm from './Authform';
 import ProtectedRoute from './ProtectedRoute'; // Import the ProtectedRoute component
 import Footer from './Footer';
 import PlantCard from './plantCard';
