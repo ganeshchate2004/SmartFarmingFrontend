6d1b0b75f3dc27190e46db58e459a70d74c7b2ff
ganeshchate2004 <ganeshchate2004@gmail.com>
2025-03-30 23:54:28 +0530

    final commit

--- src/PricePrediction.jsx
+++ src/PricePrediction.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import './PricePrediction.css';
-import priceImage from './price.png'; // Import the local image
+import './priceprediction.css'; // Import the CSS file
 
-const PricePrediction = () => {
-  return (
-    <div className="graph-container">
-      <div className="graph-card">
-        <img src={priceImage} alt="Price Prediction Graph" className="graph-image" />
-      </div>
-    </div>
-  );
-};
+const PricePrediction = ({ url }) => {
+    const plantImages = [
+        "https://indianpotato.com/wp-content/uploads/2025/02/Potato-Price-Jan-2025-final-scaled.jpg"]
+      return (
+        <div className="graph-container">
+          {plantImages.map((image, index) => (
+            <div key={index} className="graph-card">
+              <img src={image} alt={`Plant ${index + 1}`} className="graph-image" />
+            </div>
+          ))}
+        </div>
+      );
+    };
+export default PricePrediction;/* plantCard.css */
 
-export default PricePrediction;
